8a687451e379a34c5e1a010173ae104e01a8cd81
DanielMock <daniel.alexander.mock@rwth-aachen.de>
2016-10-10 21:30:45 +0200

    Fine tuning themeing + layout Next: ThemeDictionary

---
+++ EasyMensa/Converters/DateToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace EasyMensa.Converters
+{
+	public class DateToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, String culture)
+		{
+			if (targetType != typeof(string))
+				throw new InvalidOperationException("The target must be a String");
+
+			DateTime date = (DateTime)value;
+			return date.ToString("m");
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- EasyMensa/Models/Meal.cs
+++ EasyMensa/Models/Meal.cs
@@ -19,8 +19,9 @@ namespace EasyMensa.Models
 			get { return _name; }
 			set // Filters out shitty spaces and splits the name and the description of a meal
 			{
-				_name = Regex.Replace(value, @" ( |,)", " ");
-				var substrings = Regex.Split(_name, "(mit|und)");
+				value = Regex.Replace(value, @"  ", " ");
+				value = Regex.Replace(value, @" ,", ",");
+				var substrings = Regex.Split(value, "(mit|und)");
 				_name = substrings[0];
 				substrings[0] = "";
 				Description = String.Join("", substrings);
