f8519902d8c7aebb3b441b7778177d72f36d0f7f
DanielMock <daniel.alexander.mock@rwth-aachen.de>
2017-01-15 12:34:22 +0100

    Multimensaunterstützung rudimentär fertig

--- EasyMensa/MainPage.xaml.cs
+++ EasyMensa/MainPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Security.Cryptography.X509Certificates;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using EasyMensa.Models;
+using EasyMensa.ViewModels;
 
 // Die Vorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 dokumentiert.
 
@@ -10,13 +13,21 @@ namespace EasyMensa
     /// </summary>
     public sealed partial class MainPage : Page
     {
-		public NotifyTaskCompletion<Canteen> MensaAcademica { get; private set; }
-
 		public MainPage()
         {
-			MensaAcademica = new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(187));
-
             this.InitializeComponent();
+
+	        DataContextChanged += (s, e) =>
+	        {
+		        ViewModel = DataContext as CanteenCollectionViewModel;
+	        };
         }
+
+	    public CanteenCollectionViewModel ViewModel { get; set; }
+
+	    private void HamburgerButton_OnClick(object sender, RoutedEventArgs e)
+	    {
+		    MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+	    }
     }
 }
---
+++ EasyMensa/ViewModels/CanteenCollectionViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using EasyMensa.Models;
+using GalaSoft.MvvmLight;
+
+namespace EasyMensa.ViewModels
+{
+	public class CanteenCollectionViewModel : ViewModelBase
+	{
+		public ObservableCollection<NotifyTaskCompletion<Canteen>> canteenCollection;
+
+		private NotifyTaskCompletion<Canteen> _selectedCanteen;
+		public NotifyTaskCompletion<Canteen> SelectedCanteen
+		{
+			get { return _selectedCanteen; }
+			set { _selectedCanteen = value; RaisePropertyChanged();}
+		}
+
+		public CanteenCollectionViewModel()
+		{
+			canteenCollection = new ObservableCollection<NotifyTaskCompletion<Canteen>>
+			{
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(187)),  //  Academica
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(95)),  //  Ahorn
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(96)),  //  Vita
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(94)),  //  Templergraben
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(93)),  //  Cafete
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(99)),  //  Goethe
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(98)),  //  EUPS
+				new NotifyTaskCompletion<Canteen>(OpenMensaFetcher.GetCanteenAysnc(187))  //  Bayernalle
+			};
+
+			SelectedCanteen = canteenCollection[0];
+		}
+	}
+}
